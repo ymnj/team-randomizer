4080a654a555a1f7fd73d5db882ee3860f2aa599
Tom <tomhung87@gmail.com>
2016-01-31 21:38:55 -0800

    Add method for num_per_team selection

--- method.rb
+++ method.rb
@@ -10,24 +10,21 @@ class Split
 		# If the number of individuals do not divide evenly by the desired number
 		if(group_array.length % divide_by_num != 0)
 
-			team_array = group_array.each_slice( num_per_team ).to_a
-			# [one, two, three, four, five, six, seven, eight, nine, ten]
-			# 
+	
 			# This multi-array will contain one extra array with the left over individuals. Loop through this last array and push them into random "correct" arrays
-			# 
-			
+			team_array = group_array.each_slice( num_per_team ).to_a
+	
+			# Split array into wanted number of arrays and leftover arrays
 			correct_teams = team_array[0..divide_by_num - 1]
-			#[["one"], ["two"], ["three"], ["four"], ["five"], ["six"], ["seven"]]
 			left_over = team_array[divide_by_num..team_array.length].flatten
-			#[["eight"], ["nine"], ["ten"]]
-
-			i = 0
 			
-			left_over.size.times do
 
+			# Loop through left over array and insert them into a loop of correct arrays
+			i = 0
+
+			left_over.size.times do
 				correct_teams[i] << left_over.pop
 				i += 1
-
 			end
 
 			correct_teams
@@ -38,19 +35,36 @@ class Split
 
 		end	
 
-	end
+	end # Ed divide_by_team
 
 
 
-	def self.numPerTeam(group_array, divide_by_num)
+	def self.num_per_team(group_array, divide_by_num)
 
-		# if(numOfIndividuals / number != 0)
+		# If the number of individuals do not divide evenly by the desired number
+		if(group_array.length % divide_by_num != 0)
 
-		# else
+			teams = []
+			counter = 0
+			mid = (group_array.length / divide_by_num)
 
-		# end	
+			1.upto(divide_by_num) do
+				|i|
+				last = counter + mid
+				last = last-1 unless group_array.length % divide_by_num >= i
+				teams << group_array[counter..last] || []
+				counter = last + 1
 
+			end
 
-	end
+			teams
+
+		else # Teams will be divided evenly
+
+			group_array.each_slice( divide_by_num ).to_a
+
+		end	
+
+	end # End num_per_team
 
 end
--- team_app.rb
+++ team_app.rb
@@ -13,19 +13,21 @@ end
 
 post '/teampicker' do 
 
-	@arr_of_names = params[:list].split( /,\s*/ ).shuffle
+	@name_list = params[:list]
+	@arr_of_names = @name_list.split( /,\s*/ )
 	#returns an array of names split/shuffled
 
 	@method = params[:method]	##divide by team or individials per team
 	@number = params[:number].to_i #number to be divided by
 
-	#@total_individuals = @list_of_names.length
-
 	if(@method == "num_of_team")
-		@result = Split.divide_by_team(@arr_of_names, @number)
+
+		@result = Split.divide_by_team(@arr_of_names.shuffle, @number)
 
 	else (@method == "num_per_team")
-		@result = Split.numPerTeam(@arr_of_names, @number)
+
+		@result = Split.num_per_team(@arr_of_names.shuffle, @number)
+
 	end
 
 	p @result
